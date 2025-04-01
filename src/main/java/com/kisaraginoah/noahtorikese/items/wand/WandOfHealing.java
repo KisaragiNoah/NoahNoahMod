acdf0aa03a171d1128e2f732b3c20990ec943c02
kisaraginoah <kouta19501821@gmail.com>
2025-04-01 16:31:35 +0900

    Wand Of Healing Test

--- src/main/java/com/kisaraginoah/noahtorikese/items/wand/WandOfHealing.java
+++ src/main/java/com/kisaraginoah/noahtorikese/items/wand/WandOfHealing.java
@@ -1,10 +1,18 @@
 package com.kisaraginoah.noahtorikese.items.wand;
 
+import net.minecraft.world.entity.LivingEntity;
 import net.minecraft.world.item.Item;
+import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.item.Rarity;
+import net.minecraft.world.level.Level;
 
 public class WandOfHealing extends Item {
     public WandOfHealing() {
         super(new Item.Properties().stacksTo(1).rarity(Rarity.UNCOMMON));
+    }
+
+    @Override
+    public ItemStack finishUsingItem(ItemStack stack, Level level, LivingEntity livingEntity) {
+        return stack;
     }
 }
