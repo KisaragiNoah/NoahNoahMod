5d4fb3242f89772f0d52c130e704683d1ba2d2b4
kisaraginoah <kouta19501821@gmail.com>
2025-04-08 08:55:55 +0900

    パーティクル量の増加　＆　プレイヤーが地面にいるときのみチャージ可能に変更

--- src/main/java/com/kisaraginoah/noahtorikese/effects/HighjumpEffect.java
+++ src/main/java/com/kisaraginoah/noahtorikese/effects/HighjumpEffect.java
@@ -18,19 +18,27 @@ public class HighjumpEffect extends MobEffect {
 
     @Override
     public boolean applyEffectTick(@NotNull LivingEntity entity, int amplifier) {
-        if (entity instanceof Player player && player.level().isClientSide) {
+        if (entity instanceof Player player && player.level().isClientSide && player.onGround()) {
             if (player.isShiftKeyDown()) {
                 ShiftPressTime++;
-                if (ShiftPressTime % 5 == 0) {
-                    player.playSound(SoundEvents.EGG_THROW, 0.7f, 0.6f);
-                    player.level().addParticle(ParticleTypes.CLOUD, true, player.getX(), player.getY(), player.getZ(), 0, 0, 0);
-                }
-            } else if (ShiftPressTime >= 5 && player.onGround()) {
+            } else if (ShiftPressTime >= 5) {
                 double jumpHeight = Math.floor(Math.min((double)ShiftPressTime / 5, 40.0));
                 double MotionY = 0.42D + (jumpHeight * 0.1);
                 player.setDeltaMovement(new Vec3(player.getDeltaMovement().x(), MotionY, player.getDeltaMovement().z()));
                 player.hasImpulse = true;
                 ShiftPressTime = 0;
+            }
+            if (ShiftPressTime % 5 == 0 && ShiftPressTime != 0) {
+                player.playSound(SoundEvents.EGG_THROW, 0.7f, 0.6f);
+                player.level().addParticle(ParticleTypes.CLOUD, true, player.getX(), player.getY(), player.getZ(), 0, 0, 0);
+                player.level().addParticle(ParticleTypes.CLOUD, true, player.getX() + 0.1, player.getY(), player.getZ(), 0, 0, 0);
+                player.level().addParticle(ParticleTypes.CLOUD, true, player.getX() - 0.1, player.getY(), player.getZ(), 0, 0, 0);
+                player.level().addParticle(ParticleTypes.CLOUD, true, player.getX() + 0.1, player.getY(), player.getZ() + 0.1, 0, 0, 0);
+                player.level().addParticle(ParticleTypes.CLOUD, true, player.getX() + 0.1, player.getY(), player.getZ() - 0.1, 0, 0, 0);
+                player.level().addParticle(ParticleTypes.CLOUD, true, player.getX(), player.getY(), player.getZ() + 0.1, 0, 0, 0);
+                player.level().addParticle(ParticleTypes.CLOUD, true, player.getX(), player.getY(), player.getZ() - 0.1, 0, 0, 0);
+                player.level().addParticle(ParticleTypes.CLOUD, true, player.getX() + 0.1, player.getY(), player.getZ() + 0.1, 0, 0, 0);
+                player.level().addParticle(ParticleTypes.CLOUD, true, player.getX() - 0.1, player.getY(), player.getZ() + 0.1, 0, 0, 0);
             }
         }
         return super.applyEffectTick(entity, amplifier);
