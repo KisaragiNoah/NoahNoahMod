284ac3450bbb52c5dad993d12e6fa9f1d8bd30bf
BuildTools <unconfigured@null.spigotmc.org>
2025-04-07 21:10:23 +0900

    SkateとHighJumpにミスがあったので一度削除

--- src/main/java/com/kisaraginoah/noahtorikese/effects/SkateEffect.java
+++ src/main/java/com/kisaraginoah/noahtorikese/effects/SkateEffect.java
@@ -1,10 +1,8 @@
 package com.kisaraginoah.noahtorikese.effects;
 
-import net.minecraft.network.chat.Component;
 import net.minecraft.world.effect.MobEffect;
 import net.minecraft.world.effect.MobEffectCategory;
 import net.minecraft.world.entity.LivingEntity;
-import net.minecraft.world.phys.Vec3;
 
 import javax.annotation.ParametersAreNonnullByDefault;
 
@@ -16,26 +14,6 @@ public class SkateEffect extends MobEffect {
 
     @Override
     public boolean applyEffectTick(LivingEntity entity, int amplifier) {
-        if (!entity.onGround()) return false;
-
-        Vec3 motion = entity.getDeltaMovement();
-        boolean isMoving = motion.horizontalDistanceSqr() != 0;
-
-        if (isMoving) {
-            double slideFactor = 1.15 + amplifier * 0.15;
-
-            entity.sendSystemMessage(Component.literal("X:"+ motion.x + " Y:" + motion.y + " Z:" + motion.z));
-            System.out.println("X:" + motion.x + " Y:" + motion.y + " Z:" + motion.z);
-
-            Vec3 newMotion = new Vec3(
-                    motion.x * slideFactor,
-                    motion.y,
-                    motion.z * slideFactor
-            );
-
-            entity.setDeltaMovement(newMotion);
-            entity.hasImpulse = true;
-        }
         return super.applyEffectTick(entity, amplifier);
     }
 
--- src/main/java/com/kisaraginoah/noahtorikese/init/ModEffect.java
+++ src/main/java/com/kisaraginoah/noahtorikese/init/ModEffect.java
@@ -2,6 +2,7 @@ package com.kisaraginoah.noahtorikese.init;
 
 import com.kisaraginoah.noahtorikese.NoahNoahMod;
 import com.kisaraginoah.noahtorikese.effects.ClimeBlockEffect;
+import com.kisaraginoah.noahtorikese.effects.HighjumpEffect;
 import com.kisaraginoah.noahtorikese.effects.SkateEffect;
 import net.minecraft.core.Holder;
 import net.minecraft.core.registries.BuiltInRegistries;
@@ -19,5 +20,5 @@ public class ModEffect {
             "skate_effect", () -> new SkateEffect(MobEffectCategory.NEUTRAL, 0xf800f8));
 
     public static final Holder<MobEffect> HIGHJUMP_EFFECT = REGISTER.register(
-            "highjump_effect", () -> new SkateEffect(MobEffectCategory.NEUTRAL, 0xf800f8));
+            "highjump_effect", () -> new HighjumpEffect(MobEffectCategory.NEUTRAL, 0xf85348));
 }
