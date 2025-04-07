73b6f7842cc14edb3232dbc0c26e5e396bfc0d6f
BuildTools <unconfigured@null.spigotmc.org>
2025-04-07 21:41:05 +0900

    とりあえずHighJumpが動作するように変更

--- src/main/java/com/kisaraginoah/noahtorikese/effects/HighjumpEffect.java
+++ src/main/java/com/kisaraginoah/noahtorikese/effects/HighjumpEffect.java
@@ -3,6 +3,8 @@ package com.kisaraginoah.noahtorikese.effects;
 import net.minecraft.world.effect.MobEffect;
 import net.minecraft.world.effect.MobEffectCategory;
 import net.minecraft.world.entity.LivingEntity;
+import net.minecraft.world.entity.player.Player;
+import net.minecraft.world.phys.Vec3;
 import org.jetbrains.annotations.NotNull;
 
 public class HighjumpEffect extends MobEffect {
@@ -10,8 +12,21 @@ public class HighjumpEffect extends MobEffect {
         super(category, color);
     }
 
+    private static int ShiftPressTime = 0;
+
     @Override
     public boolean applyEffectTick(@NotNull LivingEntity entity, int amplifier) {
+        if (entity instanceof Player player && player.level().isClientSide) {
+            if (player.isShiftKeyDown()) {
+                ShiftPressTime++;
+            } else if (ShiftPressTime >= 5 && player.onGround()) {
+                double jumpHeight = Math.floor(Math.min((double)ShiftPressTime / 10, 50.0));
+                double MotionY = 0.42D + (jumpHeight * 0.1);
+                player.setDeltaMovement(new Vec3(player.getDeltaMovement().x(), MotionY, player.getDeltaMovement().z()));
+                player.hasImpulse = true;
+                ShiftPressTime = 0;
+            }
+        }
         return super.applyEffectTick(entity, amplifier);
     }
 
