5cd3f38e201b1185e38c92c3dc3adebe3aec1ed9
kisaraginoah <kouta19501821@gmail.com>
2025-04-08 11:08:01 +0900

    Skate変更

--- src/main/java/com/kisaraginoah/noahtorikese/effects/SkateEffect.java
+++ src/main/java/com/kisaraginoah/noahtorikese/effects/SkateEffect.java
@@ -3,7 +3,6 @@ package com.kisaraginoah.noahtorikese.effects;
 import net.minecraft.world.effect.MobEffect;
 import net.minecraft.world.effect.MobEffectCategory;
 import net.minecraft.world.entity.LivingEntity;
-import net.minecraft.world.phys.Vec3;
 
 import javax.annotation.ParametersAreNonnullByDefault;
 
@@ -15,15 +14,6 @@ public class SkateEffect extends MobEffect {
 
     @Override
     public boolean applyEffectTick(LivingEntity entity, int amplifier) {
-        Vec3 motion = entity.getDeltaMovement();
-        double slipperiness = 0.98;
-
-        entity.setDeltaMovement(
-                motion.x * slipperiness,
-                motion.y,
-                motion.z * slipperiness
-        );
-
         return super.applyEffectTick(entity, amplifier);
     }
 
