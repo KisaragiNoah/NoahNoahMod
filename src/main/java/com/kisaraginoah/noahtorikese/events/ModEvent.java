cb2670633bea8aefc7740e1748b3d0504dd05d9c
kisaraginoah <kouta19501821@gmail.com>
2025-04-14 11:16:44 +0900

    コード整形

--- src/main/java/com/kisaraginoah/noahtorikese/events/ModEvent.java
+++ src/main/java/com/kisaraginoah/noahtorikese/events/ModEvent.java
@@ -6,8 +6,6 @@ import net.minecraft.world.item.alchemy.PotionBrewing;
 import net.minecraft.world.item.alchemy.Potions;
 import net.neoforged.bus.api.SubscribeEvent;
 import net.neoforged.fml.common.EventBusSubscriber;
-import net.neoforged.fml.common.Mod;
-import net.neoforged.neoforge.common.brewing.BrewingRecipeRegistry;
 import net.neoforged.neoforge.event.brewing.RegisterBrewingRecipesEvent;
 
 @EventBusSubscriber
