d1aec7580e819a02df1988a8ba296259fd2e32a2
kisaraginoah <kouta19501821@gmail.com>
2025-04-17 10:34:54 +0900

    StackablePotion修正

--- src/main/java/com/kisaraginoah/noahtorikese/events/PotionEvent.java
+++ src/main/java/com/kisaraginoah/noahtorikese/events/PotionEvent.java
@@ -1,12 +1,17 @@
 package com.kisaraginoah.noahtorikese.events;
 
 import com.kisaraginoah.noahtorikese.init.ModEffect;
+import com.kisaraginoah.noahtorikese.init.ModPotion;
 import net.minecraft.resources.ResourceLocation;
 import net.minecraft.world.entity.LivingEntity;
 import net.minecraft.world.entity.ai.attributes.AttributeModifier;
 import net.minecraft.world.entity.ai.attributes.Attributes;
+import net.minecraft.world.item.Items;
+import net.minecraft.world.item.alchemy.PotionBrewing;
+import net.minecraft.world.item.alchemy.Potions;
 import net.neoforged.bus.api.SubscribeEvent;
 import net.neoforged.fml.common.EventBusSubscriber;
+import net.neoforged.neoforge.event.brewing.RegisterBrewingRecipesEvent;
 import net.neoforged.neoforge.event.tick.EntityTickEvent;
 
 import java.util.Objects;
@@ -17,6 +22,31 @@ public class PotionEvent {
     private static final ResourceLocation bigsizeid = ResourceLocation.fromNamespaceAndPath("noahnoahmod", "bigsize_scale");
     private static final ResourceLocation smallsizeid = ResourceLocation.fromNamespaceAndPath("noahnoahmod", "smallsize_scale");
     private static final ResourceLocation berserkerid = ResourceLocation.fromNamespaceAndPath("noahnoahmod", "berserker_value");
+
+    @SubscribeEvent
+    public static void onBrewingRecipeRegister(RegisterBrewingRecipesEvent event) {
+        PotionBrewing.Builder builder = event.getBuilder();
+
+        builder.addMix(Potions.LEAPING, Items.SPIDER_EYE, ModPotion.CLIMEBLOCK_POTION);
+        builder.addMix(ModPotion.CLIMEBLOCK_POTION, Items.REDSTONE, ModPotion.LONG_CLIMEBLOCK_POTION);
+        builder.addMix(Potions.SWIFTNESS, Items.BLUE_ICE, ModPotion.SKATE_POTION);
+        builder.addMix(ModPotion.SKATE_POTION, Items.REDSTONE, ModPotion.LONG_SKATE_POTION);
+        builder.addMix(Potions.LEAPING, Items.RABBIT_FOOT, ModPotion.HIGHJUMP_POTION);
+        builder.addMix(ModPotion.HIGHJUMP_POTION, Items.REDSTONE, ModPotion.LONG_HIGHJUMP_POTION);
+        builder.addMix(Potions.LEAPING, Items.SLIME_BALL, ModPotion.SLIMEBOUNCE_POTION);
+        builder.addMix(ModPotion.SLIMEBOUNCE_POTION, Items.REDSTONE, ModPotion.LONG_SLIMEBOUNCE_POTION);
+        builder.addMix(Potions.AWKWARD, Items.RED_MUSHROOM_BLOCK, ModPotion.BIGSIZE_POTION);
+        builder.addMix(ModPotion.BIGSIZE_POTION, Items.REDSTONE, ModPotion.LONG_BIGSIZE_POTION);
+        builder.addMix(Potions.AWKWARD, Items.FERMENTED_SPIDER_EYE, ModPotion.SMALLSIZE_POTION);
+        builder.addMix(ModPotion.SMALLSIZE_POTION, Items.REDSTONE, ModPotion.LONG_SMALLSIZE_POTION);
+        builder.addMix(Potions.STRENGTH, Items.FIRE_CHARGE, ModPotion.BERSERKER_POTION);
+        builder.addMix(ModPotion.BERSERKER_POTION, Items.REDSTONE, ModPotion.LONG_BERSERKER_POTION);
+        builder.addMix(Potions.AWKWARD, Items.NETHER_STAR, ModPotion.DURATION_EXTENDS_POTION);
+        builder.addMix(Potions.TURTLE_MASTER, Items.TURTLE_SCUTE, ModPotion.INCREASE_INVULNERABLE_POTION);
+        builder.addMix(ModPotion.INCREASE_INVULNERABLE_POTION, Items.REDSTONE, ModPotion.LONG_INCREASE_INVULNERABLE_POTION);
+        builder.addMix(ModPotion.INCREASE_INVULNERABLE_POTION, Items.FERMENTED_SPIDER_EYE, ModPotion.DECREASE_INVULNERABLE_POTION);
+        builder.addMix(ModPotion.DECREASE_INVULNERABLE_POTION, Items.REDSTONE, ModPotion.LONG_DECREASE_INVULNERABLE_POTION);
+    }
 
     @SubscribeEvent
     public static void onEntityTick(EntityTickEvent.Post event) {
--- src/main/java/com/kisaraginoah/noahtorikese/items/potion/StackableSplashPotion.java
+++ src/main/java/com/kisaraginoah/noahtorikese/items/potion/StackableSplashPotion.java
@@ -1,10 +1,12 @@
 package com.kisaraginoah.noahtorikese.items.potion;
 
+import net.minecraft.core.component.DataComponents;
 import net.minecraft.world.item.Item;
 import net.minecraft.world.item.SplashPotionItem;
+import net.minecraft.world.item.alchemy.PotionContents;
 
 public class StackableSplashPotion extends SplashPotionItem {
     public StackableSplashPotion() {
-        super(new Item.Properties().stacksTo(16));
+        super(new Item.Properties().stacksTo(16).component(DataComponents.POTION_CONTENTS, PotionContents.EMPTY));
     }
 }
