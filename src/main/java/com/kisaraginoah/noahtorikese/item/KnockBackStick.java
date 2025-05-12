cb731580c7824a6e16e025545ed442a2ad50f800
kisaraginoah <kouta19501821@gmail.com>
2025-05-12 09:49:47 +0900

    細かい修正

--- src/main/java/com/kisaraginoah/noahtorikese/item/KnockBackStick.java
+++ src/main/java/com/kisaraginoah/noahtorikese/item/KnockBackStick.java
@@ -1,15 +1,18 @@
 package com.kisaraginoah.noahtorikese.item;
 
 import net.minecraft.MethodsReturnNonnullByDefault;
+import net.minecraft.network.chat.Component;
 import net.minecraft.world.InteractionHand;
 import net.minecraft.world.InteractionResultHolder;
 import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.item.Item;
 import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.item.Rarity;
+import net.minecraft.world.item.TooltipFlag;
 import net.minecraft.world.level.Level;
 
 import javax.annotation.ParametersAreNonnullByDefault;
+import java.util.List;
 
 @ParametersAreNonnullByDefault
 @MethodsReturnNonnullByDefault
@@ -25,7 +28,12 @@ public class KnockBackStick extends Item {
 
     @Override
     public InteractionResultHolder<ItemStack> use(Level level, Player player, InteractionHand usedHand) {
-        player.knockback(1.0F, 0, 0);
+        player.knockback(1.0F, player.getX(), player.getZ());
         return super.use(level, player, usedHand);
+    }
+
+    @Override
+    public void appendHoverText(ItemStack stack, TooltipContext context, List<Component> tooltipComponents, TooltipFlag tooltipFlag) {
+        tooltipComponents.add(Component.translatable("item.noahnoahmod.test_item.tooltip1"));
     }
 }
